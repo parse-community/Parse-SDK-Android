5bacd0821d4c286dba3c29ef49dd92175f0ffcb7
Roger Hu <roger.hu@gmail.com>
2017-03-08 17:36:50 -0800

    Bump to v1.13.3 release

--- Parse/src/main/java/com/parse/ParseObject.java
+++ Parse/src/main/java/com/parse/ParseObject.java
@@ -48,7 +48,7 @@ import bolts.TaskCompletionSource;
  */
 public class ParseObject {
   private static final String AUTO_CLASS_NAME = "_Automatic";
-  /* package */ static final String VERSION_NAME = "1.13.3-SNAPSHOT";
+  /* package */ static final String VERSION_NAME = "1.13.3";
 
   /*
   REST JSON Keys
