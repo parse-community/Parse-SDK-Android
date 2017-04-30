a9de2f44ce5d54038c70c7b5f1d6aa4ecc629f72
Michael Mimeault <michael.mimeault@gmail.com>
2017-04-30 04:14:18 -0400

    Allow the GcmBroadcastReceiver to be subclassed (#638)
    * Allow the GcmBroadcastReceiver to act as a composite so we can extend its functionnality
    
    * Rollback composite and remove final with call super annotation
    
    * Proper formatting
    
    * Change the validation to support BroadcastReceiver subclasses

--- Parse/src/main/java/com/parse/GcmBroadcastReceiver.java
+++ Parse/src/main/java/com/parse/GcmBroadcastReceiver.java
@@ -11,13 +11,15 @@ package com.parse;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.CallSuper;
 
 /**
  * @exclude
  */
 public class GcmBroadcastReceiver extends BroadcastReceiver {
   @Override
-  public final void onReceive(Context context, Intent intent) {
+  @CallSuper
+  public void onReceive(Context context, Intent intent) {
     ServiceUtils.runWakefulIntentInService(context, intent, PushService.class);
   }
 }
