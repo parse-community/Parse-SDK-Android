618594c9eb91375051004739d77213eea17abde5
Daniel San <danielsan@ilhasoft.com.br>
2018-09-26 23:19:04 -0300

    Implement generic delegate for attributes
    Signed-off-by: Daniel San <danielsan@ilhasoft.com.br>

---
+++ ktx/src/main/java/com/parse/ktx/delegates/ParseDelegate.kt
@@ -0,0 +1,26 @@
+@file:Suppress("unused", "NOTHING_TO_INLINE")
+
+package com.parse.ktx.delegates
+
+import com.parse.ParseObject
+import com.parse.ktx.getAs
+import com.parse.ktx.putOrIgnore
+import kotlin.reflect.KProperty
+
+/**
+ * Created by daniel on 9/26/18
+ */
+class ParseDelegate<T> {
+
+    @Suppress("UNCHECKED_CAST")
+    operator fun getValue(parseObject: ParseObject, property: KProperty<*>): T {
+        return parseObject.getAs(property.name)
+    }
+
+    operator fun setValue(parseObject: ParseObject, property: KProperty<*>, t: T?) {
+        parseObject.putOrIgnore(property.name, t)
+    }
+
+}
+
+inline fun <T> attribute() = ParseDelegate<T>()
