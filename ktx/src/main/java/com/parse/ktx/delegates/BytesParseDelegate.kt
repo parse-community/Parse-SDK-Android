fd89ca86211de1c0d629290b8615f09bd956fa04
Daniel San <danielsan@ilhasoft.com.br>
2018-09-26 23:50:01 -0300

    Implement specialized property delegation for bytes
    Signed-off-by: Daniel San <danielsan@ilhasoft.com.br>

---
+++ ktx/src/main/java/com/parse/ktx/delegates/BytesParseDelegate.kt
@@ -0,0 +1,24 @@
+@file:Suppress("unused", "NOTHING_TO_INLINE")
+
+package com.parse.ktx.delegates
+
+import com.parse.ParseObject
+import com.parse.ktx.putOrIgnore
+import kotlin.reflect.KProperty
+
+/**
+ * Created by daniel on 9/26/18
+ */
+class BytesParseDelegate {
+
+    operator fun getValue(parseObject: ParseObject, property: KProperty<*>): ByteArray? {
+        return parseObject.getBytes(property.name)
+    }
+
+    operator fun setValue(parseObject: ParseObject, property: KProperty<*>, value: ByteArray?) {
+        parseObject.putOrIgnore(property.name, value)
+    }
+
+}
+
+inline fun bytesAttribute() = BytesParseDelegate()
