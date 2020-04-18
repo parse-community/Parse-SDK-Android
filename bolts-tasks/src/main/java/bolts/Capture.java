3131c0c8902491d03146cd5b6d5fde1b5a0b9e8d
Roger Hu <rogerh@squareup.com>
2020-04-17 23:25:55 -0700

    Add bolts tasks to this library

---
+++ bolts-tasks/src/main/java/bolts/Capture.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package bolts;
+
+/**
+ * Provides a class that can be used for capturing variables in an anonymous class implementation.
+ *
+ * @param <T>
+ */
+public class Capture<T> {
+    private T value;
+
+    public Capture() {
+    }
+
+    public Capture(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+
+    public void set(T value) {
+        this.value = value;
+    }
+}
