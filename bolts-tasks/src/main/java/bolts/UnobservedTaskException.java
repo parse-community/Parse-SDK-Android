a1db62a4e4e633db471dfcdca51c7fdcb8713e23
Roger Hu <roger.hu@gmail.com>
2020-04-20 21:45:17 -0700

    Add bolts tasks to this library (#1018)
    Co-authored-by: Roger Hu <rogerh@squareup.com>

---
+++ bolts-tasks/src/main/java/bolts/UnobservedTaskException.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package bolts;
+
+/**
+ * Used to signify that a Task's error went unobserved.
+ */
+public class UnobservedTaskException extends RuntimeException {
+    public UnobservedTaskException(Throwable cause) {
+        super(cause);
+    }
+}
