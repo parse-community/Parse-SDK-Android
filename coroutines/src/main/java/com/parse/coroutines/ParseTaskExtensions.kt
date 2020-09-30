38997523f3ec08999545d972a4dcfdcef105c064
Phill <phill.wiggins@gmail.com>
2020-09-30 14:19:16 +0100

    feature: Coroutine Task Wrapper - Suppress unused - Correct logic for background threading

--- coroutines/src/main/java/com/parse/coroutines/ParseTaskExtensions.kt
+++ coroutines/src/main/java/com/parse/coroutines/ParseTaskExtensions.kt
@@ -1,4 +1,5 @@
 @file:JvmName("ParseTaskExtensions")
+@file:Suppress("unused")
 
 package com.parse.coroutines
 
@@ -6,26 +7,29 @@ import com.parse.boltsinternal.Task
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
 @Suppress("BlockingMethodInNonBlockingContext")
 suspend fun <T> Task<T>.suspendGet(dispatcher: CoroutineDispatcher = Dispatchers.IO) = withContext<T>(dispatcher) {
-    suspendCoroutine { continuation ->
+    return@withContext suspendCoroutine { continuation ->
+        waitForCompletion()
         if (isFaulted) {
             continuation.resumeWithException(error)
         }
-        waitForCompletion()
+        continuation.resume(result)
     }
 }
 
 @Suppress("BlockingMethodInNonBlockingContext")
 suspend fun Task<Void>.suspendRun(dispatcher: CoroutineDispatcher = Dispatchers.IO) = withContext<Unit>(dispatcher) {
-    suspendCoroutine { continuation ->
+    return@withContext suspendCoroutine { continuation ->
+        waitForCompletion()
         if (isFaulted) {
             continuation.resumeWithException(error)
         }
-        waitForCompletion()
+        continuation.resume(Unit)
     }
 }
 
