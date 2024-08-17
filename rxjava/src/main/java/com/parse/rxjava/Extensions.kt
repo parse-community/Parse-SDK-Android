192199e005b9b39783e7dc5308a847e0f28e7972
Abubkr Hago <24502430+abubkr-hago@users.noreply.github.com>
2024-08-17 08:43:26 +0400

    fix: Type argument is not within its bounds.

--- rxjava/src/main/java/com/parse/rxjava/Extensions.kt
+++ rxjava/src/main/java/com/parse/rxjava/Extensions.kt
@@ -7,7 +7,7 @@ import com.parse.boltsinternal.Task
 import io.reactivex.rxjava3.core.Completable
 import io.reactivex.rxjava3.core.Single
 
-fun <T> Task<T>.toSingle(): Single<T> {
+fun <T : Any> Task<T>.toSingle(): Single<T> {
     return Single.defer {
         this.waitForCompletion()
         if (isFaulted) {
